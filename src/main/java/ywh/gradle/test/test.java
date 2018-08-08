b8ecfcf4eeaf205195bf8e23c4393d5fdaf598c4
unknown <i01007600805@CXISHD01.CATHAY_INS.CHN>
2018-08-08 18:35:48 +0800

    second commit test

--- src/main/java/ywh/gradle/test/test.java
+++ src/main/java/ywh/gradle/test/test.java
@@ -3,6 +3,7 @@ package ywh.gradle.test;
 public class test {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("changed");
     }
 
 }
