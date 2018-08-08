ebea34b1c0f2136d73c40f8af4f6d66acf5b57a9
unknown <i01007600805@CXISHD01.CATHAY_INS.CHN>
2018-08-08 19:24:59 +0800

    third commit

--- src/main/java/ywh/gradle/test/test.java
+++ src/main/java/ywh/gradle/test/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("changed");
+        System.out.println("changed twice!");
     }
 
 }
